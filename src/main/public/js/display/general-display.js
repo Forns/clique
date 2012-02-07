10ccca8a918c8bf676e401bf9fcd1b2de49f4c69
Andrew Forney <forneyandrew@gmail.com>
2012-02-07 00:52:49 -0800

    - Refactored views such that the nav elements are now a part of the general display script

--- src/main/public/js/display/general-display.js
+++ src/main/public/js/display/general-display.js
@@ -6,8 +6,17 @@
  */
 
 $(function () {
+  var navHTML = 
+      "<button id='home-nav'>Home</button>"
+    + "<button id='about-nav'>About</button>"
+    + "<button id='api-nav'>API</button>"
+    + "<button id='download-nav'>Downloads</button>"
+    + "<button id='spec-nav'>Spectral Analysis</button>"
+    + "<button id='git-nav'>Git Repo</button>";
+  
   // Navbar setup
   $("#nav-bar")
+    .html(navHTML)
     .buttonset()
     .css("visibility", "visible");
   
